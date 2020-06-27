a3aa333a5a25c1ec1e0256a73f3b334f4ccf7fcd
Danillofp <danillo.freitas@yahoo.com.br>
2020-06-27 11:09:25 -0400

    Finalizando projeto

---
+++ Project1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string response;
+            do
+            {
+                Console.WriteLine("Enter your Product name: ");
+                string name = Console.ReadLine();
+                Console.WriteLine("Enter the price: ");
+                string input = Console.ReadLine();
+                double price = Convert.ToDouble(input);
+                double price_after_taxes;
+
+                price_after_taxes = price * 0.15 + price;
+                Console.WriteLine("price = " + price + " Total price = " + price_after_taxes);
+                Console.ReadLine();
+                Console.WriteLine("Do you want to continue? Y/N ");
+                response = Console.ReadLine();
+
+            } while (response == "y");
+
+            
+                         
+         }
+        
+     
+    }
+}
